d5da96107988ba68fbe18f40ee865c3122c39bb9
Oponjousjoe <joe.ikdura@gmail.com>
2026-03-19 15:02:29 +0100

    Add fetch all products for admin product page

---
+++ app/api/admin/products/route.ts
@@ -0,0 +1,32 @@
+import { prisma } from "@/utils/prisma";
+import { getDefaultTenant } from "@/app/lib/getDefaultTenant";
+import { getLoggedInUserId } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const userId = await getLoggedInUserId();
+  const tenant = await getDefaultTenant();
+
+  if (!userId) {
+    return NextResponse.json(
+      { message: "Unauthorized: invalid token" },
+      { status: 401 },
+    );
+  }
+  if (!tenant) {
+    throw new Error("Default tenant not found");
+  }
+
+  try {
+    const products = await prisma.product.findMany({
+      orderBy: { createdAt: "desc" },
+    });
+
+    return Response.json({ products });
+  } catch (error) {
+    return Response.json(
+      { message: "Failed to fetch products" },
+      { status: 500 },
+    );
+  }
+}
