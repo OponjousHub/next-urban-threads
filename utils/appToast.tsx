2642bd03fc44b33380d106314f1c3d97c548ebdc
Oponjousjoe <joe.ikdura@gmail.com>
2026-05-22 13:19:24 +0100

    created toast helper

---
+++ utils/appToast.tsx
@@ -0,0 +1,58 @@
+import { toast } from "sonner";
+import { AdminToast } from "@/components/ui/adminToast";
+
+export const appToast = {
+  success: (title: string, description?: string) => {
+    const duration = 4000;
+
+    toast.custom(
+      () => (
+        <AdminToast
+          type="success"
+          title={title}
+          description={description}
+          duration={duration}
+        />
+      ),
+      {
+        duration,
+      },
+    );
+  },
+
+  error: (title: string, description?: string) => {
+    const duration = 6000;
+
+    toast.custom(
+      () => (
+        <AdminToast
+          type="error"
+          title={title}
+          description={description}
+          duration={duration}
+        />
+      ),
+      {
+        duration,
+      },
+    );
+  },
+
+  warning: (title: string, description?: string) => {
+    const duration = 5000;
+
+    toast.custom(
+      () => (
+        <AdminToast
+          type="warning"
+          title={title}
+          description={description}
+          duration={duration}
+        />
+      ),
+      {
+        duration,
+      },
+    );
+  },
+};
