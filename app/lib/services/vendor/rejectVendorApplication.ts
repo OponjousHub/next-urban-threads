f0bad5db75291c588818a3eb2a55e9dd587413b1
Oponjousjoe <joe.ikdura@gmail.com>
2026-06-07 18:58:38 +0100

    Prevent reject application from rejecting approved application

--- app/lib/services/vendor/rejectVendorApplication.ts
+++ app/lib/services/vendor/rejectVendorApplication.ts
@@ -4,6 +4,21 @@ export async function rejectVendorApplication(
   applicationId: string,
   rejectionReason?: string,
 ) {
+  const application = await prisma.vendorApplication.findUnique({
+    where: {
+      id: applicationId,
+    },
+  });
+
+  if (!application) {
+    throw new Error("Application not found");
+  }
+
+  //prevent rejecting approved applications
+  if (application.status !== "PENDING") {
+    throw new Error("Only pending applications can be rejected");
+  }
+
   return prisma.vendorApplication.update({
     where: {
       id: applicationId,
