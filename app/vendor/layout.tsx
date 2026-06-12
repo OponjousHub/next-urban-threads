19cf98c96ca7cbd8632ef162f24ebeb87cfe113f
Oponjousjoe <joe.ikdura@gmail.com>
2026-06-12 08:29:49 +0100

    Add protection to vendor route in vendor layout

--- app/vendor/layout.tsx
+++ app/vendor/layout.tsx
@@ -1,19 +1,21 @@
 import VendorSidebar from "@/components/vendor/vendorSidebar";
+import { getAuthPayload } from "@/lib/server/auth";
+import { redirect } from "next/navigation";
 
-export default function VendorLayout({
+export default async function VendorLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // const auth = await getAuthPayload();
+  const auth = await getAuthPayload();
 
-  // if (!auth.userId) {
-  //   redirect("/login");
-  // }
+  if (!auth.userId) {
+    redirect("/login");
+  }
 
-  // if (auth.role !== "Vendor") {
-  //   redirect("/");
-  // }
+  if (auth.role !== "Vendor") {
+    redirect("/");
+  }
   return (
     <div className="flex min-h-screen bg-gray-50">
       <VendorSidebar />
