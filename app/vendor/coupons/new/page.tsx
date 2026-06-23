32c7b577bd298972c9cdce0d10b5022024e8387d
Oponjousjoe <joe.ikdura@gmail.com>
2026-06-23 11:37:25 +0100

    Create createCoupon.ts for creating coupons

---
+++ app/vendor/coupons/new/page.tsx
@@ -0,0 +1,19 @@
+import { getCurrentVendor } from "@/lib/vendor/getCurrentVendor";
+import VendorHeaderUI from "@/components/vendor/vendorHeader";
+import CouponForm from "@/components/coupons/coupon-form";
+
+export default async function NewCouponPage() {
+  const { vendor } = await getCurrentVendor();
+
+  return (
+    <>
+      <VendorHeaderUI
+        title="Create Coupon"
+        subtitle="Create a discount code for customers"
+        vendor={vendor}
+      />
+
+      <CouponForm vendorId={vendor?.id} />
+    </>
+  );
+}
