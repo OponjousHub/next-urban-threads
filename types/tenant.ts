57992e8ea77fbaf667c60d2e0d19fb77c768c47a
Oponjousjoe <joe.ikdura@gmail.com>
2026-08-17 00:56:03 +0100

    Fix the type error in  app/layout.tsx

--- types/tenant.ts
+++ types/tenant.ts
@@ -1,14 +1,31 @@
+// export type Tenant = {
+//   id: string;
+//   name: string;
+//   email?: string | null;
+//   slug: string;
+//   domain?: string | null;
+//   heroSubtitle: string;
+//   heroCTA: string;
+//   logo: string;
+//   currency: string;
+//   heroImage: string;
+//   heroTitle: string;
+//   storeMode: "SINGLE_VENDOR" | "MULTI_VENDOR";
+// };
 export type Tenant = {
   id: string;
   name: string;
+
   email?: string | null;
   slug: string;
   domain?: string | null;
-  heroSubtitle: string;
-  heroCTA: string;
-  logo: string;
+
+  heroSubtitle?: string | null;
+  heroCTA?: string | null;
+  logo?: string | null;
   currency: string;
-  heroImage: string;
-  heroTitle: string;
+  heroImage?: string | null;
+  heroTitle?: string | null;
+
   storeMode: "SINGLE_VENDOR" | "MULTI_VENDOR";
 };
