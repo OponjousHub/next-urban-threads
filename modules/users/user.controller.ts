9da8df2ec06a444b701bf00ca4d1269e008b6012
Oponjousjoe <joe.ikdura@gmail.com>
2026-09-23 14:09:15 +0100

    Add static async createAdmin to user.controller for creating admin user

--- modules/users/user.controller.ts
+++ modules/users/user.controller.ts
@@ -1,7 +1,12 @@
 import { RegisterInput } from "@/modules/users/user.schema";
 import { UserService } from "./user.service";
+import { CreateAdminInput } from "@/modules/users/admin.schema";
 
 class UserController {
+  static async createAdmin(data: CreateAdminInput) {
+    return UserService.createAdmin(data);
+  }
+
   static async register(data: RegisterInput) {
     const user = await UserService.register(data);
     return user;
